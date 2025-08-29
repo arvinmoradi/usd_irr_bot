36c80b5914891a21f08449b96c37abab6b2417db
ArM <m.dari401@gmail.com>
2025-08-29 10:59:11 +0330

    Add Persian date and improve currency/gold/crypto formatting
    Introduces a date_time() function to display the current Persian date and weekday. Enhances price_currency, price_gold, and price_crypto functions to include Persian names, flags, and improved formatting for better readability. Updates sender.py and test.py to use the new date and formatting, and adds CNY to the currency list.

--- sender.py
+++ sender.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from bot import price_currency, price_gold, price_crypto
+from main import price_currency, price_gold, price_crypto, date_time
 import telebot
 
 load_dotenv()
@@ -9,12 +9,13 @@ CHANNEL_ID = os.getenv('CHANNEL_ID')
 bot = telebot.TeleBot(API_TOKEN)
 
 def main():
-    my_message = '<b><i>-------⬇️ Currency ⬇️--------</i></b>\n\n'
+    my_message = date_time()
+    my_message += '\n<b>-------⬇️ نرخ ارز ⬇️--------</b>\n\n'
     my_message += price_currency()
-    my_message += '\n<b><i>-------⬇️ Gold ⬇️--------</i></b>\n\n'
-    my_message += price_gold()
-    my_message += '\n<b><i>-------⬇️ Crypto ⬇️--------</i></b>\n\n'
+    my_message += '\n<b>-------⬇️ قیمت ارزهای دیجیتال ⬇️--------</b>\n\n'
     my_message += price_crypto()
+    my_message += '\n<b>-------⬇️ قیمت طلا ⬇️--------</b>\n\n'
+    my_message += price_gold()
     bot.send_message(CHANNEL_ID, my_message, parse_mode='HTML')
 
 if __name__ == '__main__':
